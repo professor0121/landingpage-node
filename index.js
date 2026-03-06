835328c465f87b01a9da2205fd10ad8ae1f34e8a
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2026-03-06 21:23:41 +0530

    test case for running

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const path = require("path");
-const sendMail=require("./services/smtp.service.js")
+const sendMail = require("./services/smtp.service.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/submit-form",async (req, res) => {
-    await sendMail(req.body);
-  res.sendFile(path.join(__dirname,"public","submit.html"))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/submit-form", async (req, res) => {
+  await sendMail(req.body);
+
+  res.sendFile(path.join(__dirname, "public", "submit.html"));
 });
 
 app.listen(PORT, () => {
