8773ce5d5918ad85b6fd2d2cc2f73302c58a3a97
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2026-03-06 21:07:31 +0530

    node version of landingpage

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const sendMail=require("./services/smtp.service.js")
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/submit-form",async (req, res) => {
+    await sendMail(req.body);
+  res.sendFile(path.join(__dirname,"public","submit.html"))
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ services/smtp.service.js
@@ -0,0 +1,90 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
+
+const sendMail = async (data) => {
+
+    const html = `
+  <div style="font-family:Arial;padding:20px">
+    <h2>New Form Submission</h2>
+
+    <table border="1" cellpadding="10" cellspacing="0" style="border-collapse:collapse;width:100%">
+      <tr>
+        <th align="left">Name</th>
+        <td>${data.name || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Email</th>
+        <td>${data.Email || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Phone</th>
+        <td>${data.Phone || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Website</th>
+        <td>${data["Website-URL"] || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Ads Spends</th>
+        <td>${data["Ads-Spends"] || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Heard About Us</th>
+        <td>${data["Where-did-you-get-to-hear-about-us"] || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Urgency</th>
+        <td>${data.Urgency || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Problem List</th>
+        <td>${data["Problem-Statements-List"] || ""}</td>
+      </tr>
+
+      <tr>
+        <th align="left">Product Page</th>
+        <td>${data["Product-Page-Link"] || ""}</td>
+      </tr>
+
+    </table>
+
+    <p style="margin-top:20px;color:gray">
+      This lead was submitted from the website form.
+    </p>
+
+  </div>
+  `;
+
+    try {
+        const info = await transporter.sendMail({
+            from: `"Website Form Resposne" ${process.env.SMTP_USER}`,
+            to: process.env.SMTP_USER,
+            subject: "New Website Lead",
+            html
+        });
+
+        console.log("Email sent:", info.messageId);
+
+    } catch (error) {
+        console.error("Email error:", error);
+    }
+};
+
+module.exports = sendMail;
