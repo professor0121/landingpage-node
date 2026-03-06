a14f7f4d401cf58afec6e0b42edf05869b51f0a1
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2026-03-06 21:21:13 +0530

    fatal error is now being solved

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const sendMail=require("./services/smtp.service.js")
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
